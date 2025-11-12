5567daa53640f80774f7cccc6da403bc94509033
KrystianOg <krystian@ogonowski.it>
2025-11-12 15:19:31 +0100

    preconfig i18n

--- services/web-nuxt/nuxt.config.ts
+++ services/web-nuxt/nuxt.config.ts
@@ -1,15 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxt/scripts',
-    '@nuxtjs/i18n'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+    "@nuxt/scripts",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    locales: [
+      {
+        code: "pl",
+        name: "Polski",
+        file: "pl.json",
+      },
+      {
+        code: "en",
+        name: "English",
+        file: "en.json",
+      },
+    ],
+    defaultLocale: "pl",
+  },
+});
