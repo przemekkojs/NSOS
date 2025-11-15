8e1373c147b7fd928d7f06912236d3062a7749f0
KrystianOg <krystian@ogonowski.it>
2025-11-15 17:40:54 +0100

    django stuff

--- services/core-api/main/users/serializers.py
+++ services/core-api/main/users/serializers.py
@@ -3,9 +3,19 @@ from .models import User, Lecturer, Student
 
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = serializers.SerializerMethodField()
+    permissions = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'groups', 'permissions', 'is_staff', 'is_superuser']
+
+    def get_groups(self, obj):
+        """Return list of group names"""
+        return [group.name for group in obj.groups.all()]
+
+    def get_permissions(self, obj):
+        return sorted(list(obj.get_all_permissions()))
 
 
 class LecturerSerializer(serializers.ModelSerializer):
