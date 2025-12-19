488642026132bbb7beb98a7d7681f9105f50875c
przemekkojs <przemek.kojs@gmail.com>
2025-12-19 08:51:52 +0100

    FastAPI Implementation

---
+++ models/app/test_request.py
@@ -0,0 +1,7 @@
+import requests
+
+if __name__ == '__main__':
+    print(requests.post(
+        "http://127.0.0.1:8000/chat",
+        params={"question": "Kto stworzył system NSOS"}
+    ).json())
