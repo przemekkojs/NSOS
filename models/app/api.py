84965ac488f4a7fd52da9a4a43a515ea2fde870b
Krystian Ogonowski <krystian@ogonowski.it>
2025-12-19 15:15:27 +0100

    answer streaming and updated requirements

--- models/app/api.py
+++ models/app/api.py
@@ -1,9 +1,11 @@
 from fastapi import Request, APIRouter
+from fastapi.responses import StreamingResponse
 
-from .llm import generate, generate_prompt
+from .llm import generate, generate_prompt, generate_stream
 from .names import LLM_4
 
 router = APIRouter()
+
 
 @router.post("/chat")
 async def chat(request: Request, question: str):
@@ -18,3 +20,18 @@ async def chat(request: Request, question: str):
     answer = generate(prompt, LLM_4)
 
     return {"answer": answer}
+
+
+@router.post("/chat-stream")
+async def chat_stream(request: Request, question: str):
+    rag = request.app.state.rag
+
+    if not question.strip():
+        return {"answer": "Przepraszam, nie znam odpowiedzi na to pytanie."}
+
+    retrieved = rag.retrieve(question)
+    prompt = generate_prompt(question, retrieved)
+
+    return StreamingResponse(
+        generate_stream(prompt, LLM_4), media_type="text/event-stream"
+    )
