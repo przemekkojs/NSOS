15c10d6c7bd1e71083f5f9d5999aa58f1354edcd
przemekkojs <przemek.kojs@gmail.com>
2026-01-09 07:48:39 +0100

    Simple web ui

--- models/app/main.py
+++ models/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from contextlib import asynccontextmanager
 
 from docs import get_docs
@@ -16,6 +18,15 @@ print("Models service starting...")
 
 load_dotenv()
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 print("Models service up!")
